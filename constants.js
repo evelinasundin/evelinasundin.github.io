bdd4e582601151dcb43a72774a92c9312d8989cb
evelinasundin <evelinasundin@hotmail.se>
2020-10-07 22:58:13 +0200

    test

---
+++ constants.js
@@ -0,0 +1,91 @@
+export const WORK =
+  [
+    {
+      id: 1,
+      title: 'Pontus Rudolfson',
+      subtitle: 'Made for Pontus Rudolfson',
+      image: require("@/assets/images/rudolfson.jpg?webp"),
+      link: 'https://www.rudolfson.com/',
+      text: '<p>My friend <a href="https://andreasantonsson.dev/" target="_blank" rel="noopener noreferrer">Andreas</a> and I co-developed this Website for photographer Pontus Rudolfson. Design by <a href="https://andreasantonsson.dev/" target="_blank" rel="noopener noreferrer">Andreas</a>. The site won <a href="https://www.awwwards.com/sites/pontus-rudolfson" target="_blank">Awwwards</a> Mobile Site of the week and site of the day on <a href="https://www.cssdesignawards.com/sites/pontus-rudolfson/35598/" target="_blank">CSS design awards</a>.<p>',
+      techniques: [
+        'Vue', 'Nuxt', 'GSAP', 'SASS', 'etc'
+      ],
+    },
+    {
+      id: 2,
+      title: 'Trickle',
+      subtitle: 'Made for ohmy',
+      link: 'https://www.wearetrickle.com/',
+      text: '<p>Website made for content distrubution agency Trickle.</p>',
+      techniques: [
+        'Javascript', 'PHP', 'Wordpress', 'SASS', 'Bodymovin', 'etc'
+      ],
+      image: require("@/assets/images/trickle.jpg?webp")
+    },
+    {
+      id: 3,
+      title: 'Highnote',
+      subtitle: 'My exam work @Nackademin',
+      link: 'http://highnote.herokuapp.com/',
+      text: '<p>My exam work that I did @Nackademin. Sign in with Spotify and see your most listened to artist, songs and create your personal playlist.</p>',
+      techniques: [
+        'React', 'Spotify API', 'OAuth', 'Heroku', 'SASS', 'etc'
+      ],
+      image: require("@/assets/images/highnote.jpg?webp")
+    },
+    {
+      id: 4,
+      title: 'Cupole - Handbook',
+      subtitle: 'Made for @ohmy',
+      link: 'https://www.life.cupole.se/',
+      text: '<p>Handbook as website made for Management Consultants agancy Cupole</p>',
+      techniques: [
+        'Javascript', 'PHP', 'Wordpress', 'SASS', 'etc'
+      ],
+      image: require("@/assets/images/cupole.jpg?webp")
+    },
+    {
+      id: 5,
+      title: 'Cultur',
+      subtitle: 'Made for @ohmy',
+      link: 'https://www.culturbar.se/',
+      text: '<p>Website made for restaurant Cultur in Gamla Stan</p>',
+      techniques: [
+        'Javascript', 'PHP', 'Wordpress', 'SASS', 'etc'
+      ],
+      image: require("@/assets/images/cultur.jpg?webp")
+    },
+    {
+      id: 6,
+      title: 'COTF - Framtidsföretagen',
+      subtitle: 'Made for @ohmy',
+      link: 'https://www.cotf.se/framtidsforetagen',
+      text: '<p>Multisite solution for Universum, for their different sites that research abouts the most popular companys in Sweden</p>',
+      techniques: [
+        'PHP', 'Multisite', 'Javascript', 'Wordpress', 'SASS', 'etc'
+      ],
+      image: require("@/assets/images/universum.jpg?webp")
+    },
+    {
+      id: 7,
+      title: 'Developers Bookmark',
+      subtitle: 'Work as a student @Nackademin',
+      link: 'https://build-yvhmdluhgo.now.sh/',
+      text: '<p>The main focus of the task was to build an application in React that collects and stores data from an own setup database in Firebase. Demands were to be able to log in through firebase authentication system and store information about users and user data.</p>',
+      techniques: [
+        'React', 'Firebase', 'Javascript', 'Bootstrap', 'SASS', 'etc'
+      ],
+      image: require("@/assets/images/dev-bookmark.jpg?webp")
+    },
+    {
+      id: 8,
+      title: 'Album Collection',
+      subtitle: 'Work as a student @Nackademin',
+      link: 'https://evelinasundin.github.io/AlbumCollection/',
+      text: '<p>The task was to create an application that collects data from an open API and to show this via HTML DOM. Some of the requirements were, among other things, to collect data from an open API that serves data in form of json, to have good code structure and use of namespaces as well as error handling.</p>',
+      techniques: [
+        'Module Pattern', 'Ajax', 'Javascript', 'Bootstrap', 'SASS', 'etc'
+      ],
+      image: require("@/assets/images/ac.jpg?webp")
+    },
+]
